96d9c6130c97449fbb7c8d9db467a5c41fa9a7fb
3verli <evanliege@gmx.fr>
2026-05-29 11:16:32 +0200

    Update 01-INSTALL AD .ps1

--- SCRIPT POWESHELL/01-INSTALL AD .ps1
+++ SCRIPT POWESHELL/01-INSTALL AD .ps1
@@ -1,10 +1,10 @@
-# Installer le rôle AD DS + outils de gestion
+# Instalation du  rôle AD DS et les  outils de gestion
 Install-WindowsFeature -Name AD-Domain-Services `
 -IncludeManagementTools `
 -Verbose
-# Mot de passe DSRM (Directory Services Restore Mode)
+# password DSRM (Directory Services Restore Mode)
 $dsrmPwd = Read-Host -AsSecureString 'Mot de passe DSRM'
-# Promouvoir le serveur en DC de la nouvelle forêt lab.local
+# Promotion du serveur en DC de la nouvelle forêt lab.local
 Install-ADDSForest `
 -DomainName 'lab.local' `
 -DomainNetbiosName 'LAB' `
